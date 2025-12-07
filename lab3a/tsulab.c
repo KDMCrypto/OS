9916c2eaab1313b0ee400cb1f4df2dc9a8e75da3
KDMCrypto <dima.korepanov.05@list.ru>
2025-12-07 19:03:43 +0700

    lab3a

---
+++ lab3a/tsulab.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init tsu_init(void)
+{
+    pr_info("Welcome to the Tomsk State University\n");
+    return 0;
+}
+
+static void __exit tsu_exit(void)
+{
+    pr_info("Tomsk State University forever!\n");
+}
+
+module_init(tsu_init);
+module_exit(tsu_exit);
+MODULE_LICENSE("GPL");
